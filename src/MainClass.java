643eeeec7e4700fa16487e94c9563438e6d1ed01
Roberto.Mazza <Roberto.Mazza@NBDE154.lutech.lan>
2022-08-27 11:25:20 +0200

    Aggiunta nuovo prodotto

--- src/MainClass.java
+++ src/MainClass.java
@@ -11,6 +11,7 @@ public class MainClass {
 		proprietaProdotto.add(proprie);
 		proprietaProdotto.add(proprieta1);
 		Prodotto prodotto = new Prodotto("barilla", "pasta", 15, "pasta integrale", proprietaProdotto, 5);
+		Prodotto prodotto1=new Prodotto("mulino bianco","biscotti",4,"Biscotti integrali",proprietaProdotto,14);
 		
 	}
 
